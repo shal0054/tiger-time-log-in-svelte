1db58660938cdec3eaa33c677f93535745ebdb20
Karim Shaloh <kshaloh@gmail.com>
2024-05-02 14:10:58 -0400

    add cancel editing icon and functionality

--- src/components/DayEntry.svelte
+++ src/components/DayEntry.svelte
@@ -44,15 +44,17 @@
 <div class="day-entry">
 	<p class="day-item">{text}</p>
 	{#if editing}
-		<input type="number" bind:value={hours} style="border: {hourBorder};" />
-		<span>:</span>
-		<input type="number" bind:value={minutes} style="border: {minBorder};" />
+		<div class="edit-time">
+			<input type="number" bind:value={hours} style="border: {hourBorder};" />
+			<span id="time-colon">:</span>
+			<input type="number" bind:value={minutes} style="border: {minBorder};" />
+		</div>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="16"
 			height="16"
-			fill="currentColor"
+			fill="green"
 			class="bi bi-floppy"
 			viewBox="0 0 16 16"
 			on:click={setUpdatedTime}
@@ -62,8 +64,13 @@
 				d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"
 			/>
 		</svg>
-		<!-- TODO: give functionality to cancel icon -->
-		<img style="width: 16px;" src="./assets/cancel-icon.svg" alt="" />
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<img
+			style="width: 16px;"
+			src="./assets/cancel-icon.svg"
+			alt="Cancel Editing"
+			on:click={() => (editing = false)}
+		/>
 	{:else}
 		<p class="day-item">{time}</p>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -88,6 +95,15 @@
 		display: flex;
 		justify-content: space-around;
 		border-bottom: 0.5px solid #bfc01099;
+	}
+	.edit-time {
+		display: flex;
+		justify-content: center;
+		gap: 4px;
+	}
+	#time-colon {
+		font-size: x-large;
+		font-weight: bold;
 	}
 	.bi-pencil,
 	.bi-floppy {
