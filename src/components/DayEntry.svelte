accc70ca18c1bfd6ff1b48f1d0e5d055138f61bd
Karim Shaloh <kshaloh@gmail.com>
2024-05-02 14:02:27 -0400

    working progress

--- src/components/DayEntry.svelte
+++ src/components/DayEntry.svelte
@@ -62,7 +62,8 @@
 				d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"
 			/>
 		</svg>
-		<!-- TODO: add a cancel icon here -->
+		<!-- TODO: give functionality to cancel icon -->
+		<img style="width: 16px;" src="./assets/cancel-icon.svg" alt="" />
 	{:else}
 		<p class="day-item">{time}</p>
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
