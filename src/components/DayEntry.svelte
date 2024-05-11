390be157ee25a8e26a7873dd219bd91122c896cf
Karim Shaloh <kshaloh@gmail.com>
2024-05-11 13:44:26 -0400

    working progress

--- src/components/DayEntry.svelte
+++ src/components/DayEntry.svelte
@@ -4,53 +4,83 @@
 	export let text;
 	export let timeStr;
 	let date = new Date();
-	let editingHere = false;
-</script>
+	let editingTime = false;
+	let editingDate = false;
+	$: console.log(date);
 
-<!-- <DateInput bind:value={date} format={'yyyy-MM-dd'} /> -->
+	function cancelEditing() {
+		editing.set(false);
+		editingDate = false;
+	}
+</script>
 
 <div class="day-entry">
 	<p class="day-item text">{text}</p>
-	<div class="edit-group">
-		<p class="day-item date">{date.toDateString()}</p>
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="16"
-			height="16"
-			fill="currentColor"
-			class="bi bi-pencil"
-			viewBox="0 0 16 16"
-			on:click={() => {
-				editing.set(true);
-				editingHere = true;
-			}}
-		>
-			<path
-				d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"
+	<!-- Editing Date -->
+	{#if editingDate}
+		<div class="edit-group">
+			<DateInput bind:value={date} format={'yyyy-MM-dd'} />
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="green"
+				class="bi bi-floppy"
+				viewBox="0 0 16 16"
+				on:click={cancelEditing}
+			>
+				<path d="M11 2H9v3h2z" />
+				<path
+					d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"
+				/>
+			</svg>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				style="width: 16px;"
+				src="./assets/cancel-icon.svg"
+				alt="Cancel Editing"
+				on:click={cancelEditing}
 			/>
-		</svg>
-	</div>
-	<div class="edit-group">
-		<p class="day-item time">{timeStr}</p>
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="16"
-			height="16"
-			fill="currentColor"
-			class="bi bi-pencil"
-			viewBox="0 0 16 16"
-			on:click={() => {
-				editing.set(true);
-				editingHere = true;
-			}}
-		>
-			<path
-				d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"
+		</div>
+		<div class="edit-group">
+			<p class="day-item time">{timeStr}</p>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				on:click={() => {
+					editing.set(true);
+					editingTime = true;
+				}}
+				src="assets/edit-icon.svg"
+				alt="edit icon"
 			/>
-		</svg>
-	</div>
+		</div>
+	{:else}
+		<div class="edit-group">
+			<p class="day-item date">{date.toDateString()}</p>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				on:click={() => {
+					editing.set(true);
+					editingDate = true;
+				}}
+				src="assets/edit-icon.svg"
+				alt="edit icon"
+			/>
+		</div>
+		<div class="edit-group">
+			<p class="day-item time">{timeStr}</p>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<img
+				on:click={() => {
+					editing.set(true);
+					editingTime = true;
+				}}
+				src="assets/edit-icon.svg"
+				alt="edit icon"
+			/>
+		</div>
+	{/if}
 </div>
 
 <style>
